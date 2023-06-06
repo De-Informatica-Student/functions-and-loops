a1aec7a2011f456df1b0a42e0399d21529e36b89
Wesley van Schaijk <wschaijk@outlook.com>
2023-06-06 08:13:42 +0200

    Code

---
+++ src/goto.c
@@ -0,0 +1,26 @@
+// Variabele voor het opslaan van waardes die verplaats worden
+int returnValue = 0;
+int nextLine = 0;
+int startingLine = 0;
+
+// De main functie is het begin van het programma
+int main() {
+    // Maak een getal aan
+    int x = 5;
+
+    // Verdubbel het met de doubler
+    nextLine = &&doubled;
+    goto doubler;
+    int y = returnValue;
+
+    // Label voor een plek in het programma
+    doubled:
+        // Het programma is succesvol afgerond
+        return 0;
+
+    // Label voor een plek in het programma
+    doubler:
+        // Functie om een getal te verdubbelen
+        returnValue = x * 2;
+        goto *nextLine;
+}
---
+++ src/regular.c
@@ -0,0 +1,15 @@
+// De main functie is het begin van het programma
+int main() {
+    // Maak een getal aan en verdubbel het
+    int x = 5;
+    int y = doubler(x);
+
+    // Het programma is succesvol afgerond
+    return 0;
+}
+
+// Functie om een getal te verdubbelen
+int doubler(int x) {
+    // Geef het dubbele van het getal terug
+    return x * 2;
+}
